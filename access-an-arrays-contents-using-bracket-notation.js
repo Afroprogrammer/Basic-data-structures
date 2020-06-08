f922718a72450218265f16bc5fd898d1bd37880b
afroprogrammer <forsonfranklin@gmail.com>
2020-06-08 21:52:23 +0000

    Access an Array's Contents Using Bracket Notation challenge complted

---
+++ access-an-arrays-contents-using-bracket-notation.js
@@ -0,0 +1,17 @@
+/*let ourVariable = ourArray[0];
+// ourVariable equals "a"
+In addition to accessing the value associated with an index, you can also set an index to a value using the same notation:
+
+ourArray[1] = "not b anymore";
+// ourArray now equals ["a", "not b anymore", "c"];
+Using bracket notation, we have now reset the item at index 1 from "b", to "not b anymore".
+
+In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides "b".
+ */
+
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[[1]] = "joana";
+// Only change code above this line
+console.log(myArray);
