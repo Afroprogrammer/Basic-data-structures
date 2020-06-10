4f1532972fd74145bf85405295ed83b52312d7a8
afroprogrammer <forsonfranklin@gmail.com>
2020-06-10 16:09:27 +0000

    Modify an Object Nested Within an Object challenge completed

---
+++ modify-an-object-nested-within-an-object.js
@@ -0,0 +1,22 @@
+/*
+nestedObject has three properties: id (value is a number), date (value is a string), and data (value is an object with its nested structure). While structures can quickly become complex, we can still use the same notations to access the information we need. To assign the value 10 to the busy property of the nested onlineStatus object, we use dot notation to reference the property:
+
+nestedObject.data.onlineStatus.busy = 10;
+Here we've defined an object userActivity, which includes another object nested within it. Set the value of the online key to 45.
+
+ */
+
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+
+// Only change code below this line
+userActivity.data.online = 45;
+// Only change code above this line
+
+console.log(userActivity);
